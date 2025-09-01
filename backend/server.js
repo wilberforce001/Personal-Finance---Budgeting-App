8437ae28e5ea28194df00055e964a7e940301abd
Kipyegon Wilber <kenmisik@gmail.com>
2025-09-01 16:55:57 +0300

    Corrected a typo

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,42 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
-import categoryRoutes from './routes/categories.js'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categories.js";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 
-app.get('/API', (req, res) => {
-    res.send('API is running...');
+// ------------------- API Routes ------------------- //
+app.get("/api", (req, res) => {
+  res.send("API is running...");
 });
 
-// Routes
 app.use("/api/categories", categoryRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
+// -------------------------------------------------- //
 
-// Serve frontend
-const _filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
+// ------------------- Serve Frontend ------------------- //
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 if (process.env.NODE_ENV === "production") {
-    const frontendPath = path.join(_dirname, "../frontend/build");
-    app.use(express.static(frontendPath));
+  const frontendPath = path.join(__dirname, "./frontend/build"); // ✅ points correctly when backend is at root
+  app.use(express.static(frontendPath));
 
-    app.get("*", (req, res) => 
-    res.sendFile(path.resolve(frontendPath, "index.html")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(frontendPath, "index.html"))
+  );
 }
+// ------------------------------------------------------- //
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
