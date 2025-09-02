4ce4c61e81185b049c104e918d1297d7858b4e14
Kipyegon Wilber <kenmisik@gmail.com>
2025-09-02 15:54:09 +0300

    Replaced API with a real base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "API", // backend server
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api", // backend server
 });
 
 // Attach token automatically if it exists
