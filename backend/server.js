bdee682cf0e936d1f42babfb6b05dc60728c51a2
Kipyegon Wilber <kenmisik@gmail.com>
2025-09-02 18:29:42 +0300

    Add frontend build and update server for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,41 +6,34 @@ import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categories.js";
 import path from "path";
 import { fileURLToPath } from "url";
-import dashboardRoutes from "./routes/dashboard.js"
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Root API check
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
-// API routes
-app.use("/api/categories", categoryRoutes);
+// ------------------- API Routes ------------------- //
 app.use("/api/users", userRoutes);
-app.use("/api/dashboard", dashboardRoutes);
+app.use("/api/categories", categoryRoutes);
 
-// Serve Frontend
+// ------------------- Deployment ------------------- //
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  const frontendPath = path.join(__dirname, "build");
+  app.use(express.static(frontendPath));
 
-  // Any route not starting with /api should load frontend
-  app.get("*", (req, res) =>
-    res.sendFile(path.join(__dirname, "frontend/build/index.html"))
-  );
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(frontendPath, "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
 }
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
